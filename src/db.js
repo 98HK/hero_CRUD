f7d341029ebce423c7e1f8ef48457a6caec141e5
98HK <110665661+98HK@users.noreply.github.com>
2022-08-18 11:13:37 +0530

    ok

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGODB_URI || "mongodb+srv://demoApi:hiteshz123@cluster0.lsdwq.mongodb.net/heroDemo";
+const mongoURI = process.env.MONGODB_URI;
 
 const connectToMongo = async () => {
   try {
